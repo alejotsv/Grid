ee0f98283373b11bee358069517c6c9693ea958a
Alejandro Salas <asalas.wd@outlook.com>
2022-11-16 18:18:41 -0500

    Generate constructor

--- src/com/java21days/Grid.java
+++ src/com/java21days/Grid.java
@@ -1,6 +1,12 @@
 package com.java21days;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Grid extends JFrame {
+    Dimension dim = new Dimension(500, 500);
+
+    Grid(String title, String[] buttons){
+
+    }
 }
