29202d41f8f4a2683ccc8d45ebc214ef90511e42
Alejandro Salas <asalas.wd@outlook.com>
2022-11-16 18:45:28 -0500

    Create Grid instance in main method

--- src/com/java21days/Grid.java
+++ src/com/java21days/Grid.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class Grid extends JFrame {
     Dimension dim = new Dimension(500, 500);
 
-    Grid(String title, String[] buttons){
+    Grid(String title, String[] buttons, int rows, int columns){
         super(title);
         setSize(dim);
         setLocationRelativeTo(null);
--- src/com/java21days/Main.java
+++ src/com/java21days/Main.java
@@ -1,4 +1,11 @@
 package com.java21days;
 
+import java.awt.*;
+
 public class Main {
+    public static void main(String[] args) {
+        String[] players = { "Courtois", "Carvajal", "Militao", "Rudiger", "Alaba", "Tchouameni", "Kroos", "Valverde", "Modric", "Vinicius", "Benzema" };
+        Grid grid = new Grid("Players", players, 5, 5);
+
+    }
 }
